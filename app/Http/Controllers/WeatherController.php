6b67f51c58347880bae5cb77924aa8d8f25a864c
Jud <r.judicael@gmail.com>
2023-07-03 17:58:35 +0200

    store weather data from API in database

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Weather;
 use Illuminate\Support\Facades\Http;
 
@@ -10,25 +11,33 @@ class WeatherController extends Controller
     public function show($city)
     {
         $apiKey = env('WEATHER_API_KEY');
-
+        
         $response = Http::get("http://api.weatherapi.com/v1/forecast.json?key={$apiKey}&q={$city}&days=2&lang=fr");
-
+        
         if($response->successful()){
-
             $weatherData = $response->json();
+            if (isset ($weatherData['forecast']))
+            {
 
-            $forecastToday = $weatherData['forecast']['forecastday'][0]['hour'][12];
-            $forecastTomorrow = $weatherData['forecast']['forecastday'][1]['hour'][12];
+                $cityWeather = City::firstOrCreate(['name' => $city]);
 
-            $weather= new Weather();
 
-            return response()->json([
-                'today' => $forecastToday['condition']['text'],
-                'tomorrow' => $forecastTomorrow['condition']['text'],
-            ]);
-
+                $forecastToday = $weatherData['forecast']['forecastday'][0]['hour'][12];
+                $forecastTomorrow = $weatherData['forecast']['forecastday'][1]['hour'][12];
+    
+                $weather= new Weather();
+                $weather->city_id = $cityWeather->id;
+                $weather->today = $forecastToday['condition']['text'];
+                $weather->tomorrow = $forecastTomorrow['condition']['text'];
+                $weather->save();
+    
+                return response()->json(['ok']);
+            } else
+            {
+                return response()->json(['status' => 'failed', 'message' => 'Ville inconnue']);
+            }
         } else {
-        return response()->json('marche pas');
+        return response()->json(['status' => 'failed', 'message' => 'erreur']);
     }
 }
 
